b0d8293024a92551b6ea55162dca56f044fb0f5f
sjcco <73748496+sjcco@users.noreply.github.com>
2021-03-04 12:21:41 -0600

    Add spec for my_each_with_index

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -24,3 +24,34 @@ describe '#my_each' do
     end
   end
 end
+
+describe '#my_each_with_index' do
+  let(:array) { [1, 2, 3] }
+  context 'No block is given' do
+    it 'returns an enumerator' do
+      expect(array.my_each_with_index).to be_an(Enumerator)
+    end
+  end
+  context 'A block is given' do
+    let(:my_test) { [] }
+    let(:test) { [] }
+    let(:indices) { [] }
+
+    it 'calls the block for each element' do
+      array.my_each_with_index { |item, index| my_test << (item + 1) }
+      array.each_with_index { |item, index| test << (item + 1) }
+      expect(my_test).to eql(test)
+    end
+
+    it 'It does not modify original array' do
+      other_array = array
+      array.my_each { |item| item + 1 }
+      expect(array).to eql(other_array)
+    end
+
+    it 'Returns the indices' do
+      array.my_each_with_index { |item, index| indices << index }
+      expect(indices).to eql([0, 1, 2])
+    end
+  end
+end
